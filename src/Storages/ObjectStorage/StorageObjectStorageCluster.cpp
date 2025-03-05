8489672ab3cd58a352ddee01f289b670aa8dc4ea
Andrej Hoos <andrej.hoos@gmail.com>
2025-03-05 19:41:15 +0100

    Change object storage cluster table functions to prefer specific replicas to improve cache locality

--- src/Storages/ObjectStorage/StorageObjectStorageCluster.cpp
+++ src/Storages/ObjectStorage/StorageObjectStorageCluster.cpp
@@ -13,6 +13,7 @@
 #include <Storages/ObjectStorage/Utils.h>
 #include <Storages/ObjectStorage/StorageObjectStorageSource.h>
 #include <Storages/extractTableFunctionFromSelectQuery.h>
+#include <Storages/ObjectStorage/StorageObjectStorageStableTaskDistributor.h>
 
 
 namespace DB
@@ -20,6 +21,7 @@ namespace DB
 namespace Setting
 {
     extern const SettingsBool use_hive_partitioning;
+    extern const SettingsBool object_storage_stable_cluster_task_distribution;
 }
 
 namespace ErrorCodes
@@ -134,19 +136,38 @@ RemoteQueryExecutor::Extension StorageObjectStorageCluster::getTaskIteratorExten
         configuration, configuration->getQuerySettings(local_context), object_storage, /* distributed_processing */false,
         local_context, predicate, virtual_columns, nullptr, local_context->getFileProgressCallback(), /*ignore_archive_globs=*/true);
 
-    auto callback = std::make_shared<std::function<String()>>([iterator]() mutable -> String
+    auto cluster = getCluster(local_context);
+
+    if (local_context->getSettingsRef()[Setting::object_storage_stable_cluster_task_distribution])
     {
-        auto object_info = iterator->next(0);
-        if (!object_info)
-            return "";
+        auto task_distributor = std::make_shared<StorageObjectStorageStableTaskDistributor>(iterator, cluster);
 
-        auto archive_object_info = std::dynamic_pointer_cast<StorageObjectStorageSource::ArchiveIterator::ObjectInfoInArchive>(object_info);
-        if (archive_object_info)
-            return archive_object_info->getPathToArchive();
+        auto callback = std::make_shared<TaskIterator>(
+            [task_distributor](Connection * connection) mutable -> String {
+                return task_distributor->getNextTask(connection);
+            });
 
-        return object_info->getPath();
-    });
-    return RemoteQueryExecutor::Extension{ .task_iterator = std::move(callback) };
+        // TODO: ObjectInfoInArchive?
+
+        return RemoteQueryExecutor::Extension{ .task_iterator = std::move(callback) };
+    }
+    else
+    {
+        auto callback = std::make_shared<TaskIterator>(
+            [iterator](Connection *) mutable -> String {
+                auto object_info = iterator->next(0);
+                if (!object_info)
+                    return "";
+
+                auto archive_object_info = std::dynamic_pointer_cast<StorageObjectStorageSource::ArchiveIterator::ObjectInfoInArchive>(object_info);
+                if (archive_object_info)
+                    return archive_object_info->getPathToArchive();
+
+                return object_info->getPath();
+            });
+
+        return RemoteQueryExecutor::Extension{ .task_iterator = std::move(callback) };
+    }
 }
 
 }
